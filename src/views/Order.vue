ed2299860403232f0618522c9d692a886847c30d
phamngocthi <pnthi.it.cp@gmail.com>
2024-04-16 14:37:21 +0700

    up

---
+++ src/views/Order.vue
@@ -0,0 +1,21 @@
+<template>    
+    <greeting :title="tilte"></greeting>
+</template>
+
+<script>
+import Greeting from "@/components/Utils/Greeting.vue";
+
+export default {
+    data() {
+        return {
+            tilte: "Quản lý đơn hàng!",
+        };
+    },
+    components: {
+        Greeting,
+    },
+}
+</script>
+
+<style>
+</style>
